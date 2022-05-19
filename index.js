34bd7c2a8a41f697dbd5d1ad6ba72e43283569c3
nosferatu-bodya <kresanbohdan420@gmail.com>
2022-05-19 10:12:22 +0300

    add loadlevel function to load levels from the levels.js file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 import levels from './levels.js'
+
+loadLevel(0, levels)
+
+function loadLevel (index, levels) {
+    document.querySelector('#text').textContent = levels[index].text
+    document.querySelector('#text-field').value = ''
+}
