c700a7757b8ba9c2b02358e6fd98ac25e8efba9f
nosferatu-bodya <kresanbohdan420@gmail.com>
2022-05-19 10:18:35 +0300

    add sepereateToSpans function so i can acces each letter seperately

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 
 import levels from './levels.js'
 
+const textElement = document.querySelector('#text')
+const textFieldElement = document.querySelector('#text-field')
+
 loadLevel(0, levels)
+
+seperateToSpans(textElement)
+
+function seperateToSpans(element) {
+    let text = element.textContent
+    let res = ''
+
+    element.querySelectorAll('span').forEach(c => c.remove())
+    element.textContent = ''
+
+    for(let i = 0; i < text.length; i++){
+        res += '<span>' + text[i] + '</span>'
+    }
+
+    element.innerHTML = res
+}
 
 function loadLevel (index, levels) {
     document.querySelector('#text').textContent = levels[index].text
